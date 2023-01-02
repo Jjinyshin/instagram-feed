030057ada5f547b06460844dfae55f28407ca990
Nyeong <iwbm312@gmail.com>
2023-01-02 10:44:21 +0900

    feed에 ListView.builder 적용
    Column위젯을 ListView.builder위젯으로 감싸서 return (피드 10개로 늘리기)

--- lib/feed.dart
+++ lib/feed.dart
@@ -16,75 +16,81 @@ class _FeedState extends State<Feed> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Image.network(
-          "https://blogpfthumb-phinf.pstatic.net/MjAyMjA4MjZfMjc2/MDAxNjYxNDk3ODIzNTY2.C-68xdAph9gtJ0DFY8SlCg-EDIA9-luj2rxRRpfJI5gg.e3EDRAhef_1CcWWUesX0ey7oAfdfdY7zEbjE1e1chZYg.PNG.iwbm0312/profileImage.png",
-          height: 400,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-        Row(
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  isFavorite = !isFavorite;
-                });
-              },
-              icon: Icon(
-                (isFavorite
-                    ? CupertinoIcons.heart_fill
-                    : CupertinoIcons.heart), //material: Icons.favorite_border
-                color: isFavorite ? Colors.pink : Colors.black,
+            Image.network(
+              "https://blogpfthumb-phinf.pstatic.net/MjAyMjA4MjZfMjc2/MDAxNjYxNDk3ODIzNTY2.C-68xdAph9gtJ0DFY8SlCg-EDIA9-luj2rxRRpfJI5gg.e3EDRAhef_1CcWWUesX0ey7oAfdfdY7zEbjE1e1chZYg.PNG.iwbm0312/profileImage.png",
+              height: 400,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
+                  icon: Icon(
+                    (isFavorite
+                        ? CupertinoIcons.heart_fill
+                        : CupertinoIcons
+                            .heart), //material: Icons.favorite_border
+                    color: isFavorite ? Colors.pink : Colors.black,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(CupertinoIcons.chat_bubble),
+                  color: Colors.black,
+                ),
+                Spacer(),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      CupertinoIcons.bookmark,
+                      color: Colors.black,
+                    ))
+              ],
+            ),
+
+            // 좋아요
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "268 likes",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(CupertinoIcons.chat_bubble),
-              color: Colors.black,
+
+            // 설명
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "Stanford University - The campus is really wide and beautiful.\nThe weather was so nice and it was a very happy day 🌞",
+              ),
             ),
-            Spacer(),
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  CupertinoIcons.bookmark,
-                  color: Colors.black,
-                ))
+
+            // 날짜
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 5, 0, 10),
+              child: Text(
+                "January 1",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            )
           ],
-        ),
-
-        // 좋아요
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Text(
-            "268 likes",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-
-        // 설명
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Text(
-            "Stanford University - The campus is really wide and beautiful.\nThe weather was so nice and it was a very happy day 🌞",
-          ),
-        ),
-
-        // 날짜
-        Padding(
-          padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
-          child: Text(
-            "January 1",
-            style: TextStyle(
-              color: Colors.grey,
-            ),
-          ),
-        )
-      ],
+        );
+      },
     );
   }
 }
