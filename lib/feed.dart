0a80cf4b244e5027b7fdedc185aeeb260d114980
Nyeong <iwbm312@gmail.com>
2023-01-02 12:30:27 +0900

    ListView.builder 위치 변경, 이미지 추가
    - ListView.builder위젯 위치 feed -> home_page로 변경
    - 피드 이미지 여러 개 보여주기(url하드코딩)

--- lib/feed.dart
+++ lib/feed.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 class Feed extends StatefulWidget {
   const Feed({
     Key? key,
+    required this.imageUrl,
   }) : super(key: key);
+
+  final String imageUrl; // 이미지 경로
 
   @override
   State<Feed> createState() => _FeedState();
@@ -16,81 +19,75 @@ class _FeedState extends State<Feed> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.network(
+          widget.imageUrl,
+          height: 400,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+        Row(
           children: [
-            Image.network(
-              "https://blogpfthumb-phinf.pstatic.net/MjAyMjA4MjZfMjc2/MDAxNjYxNDk3ODIzNTY2.C-68xdAph9gtJ0DFY8SlCg-EDIA9-luj2rxRRpfJI5gg.e3EDRAhef_1CcWWUesX0ey7oAfdfdY7zEbjE1e1chZYg.PNG.iwbm0312/profileImage.png",
-              height: 400,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              icon: Icon(
+                (isFavorite
+                    ? CupertinoIcons.heart_fill
+                    : CupertinoIcons.heart), //material: Icons.favorite_border
+                color: isFavorite ? Colors.pink : Colors.black,
+              ),
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isFavorite = !isFavorite;
-                    });
-                  },
-                  icon: Icon(
-                    (isFavorite
-                        ? CupertinoIcons.heart_fill
-                        : CupertinoIcons
-                            .heart), //material: Icons.favorite_border
-                    color: isFavorite ? Colors.pink : Colors.black,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(CupertinoIcons.chat_bubble),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.chat_bubble),
+              color: Colors.black,
+            ),
+            Spacer(),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.bookmark,
                   color: Colors.black,
-                ),
-                Spacer(),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      CupertinoIcons.bookmark,
-                      color: Colors.black,
-                    ))
-              ],
-            ),
-
-            // 좋아요
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                "268 likes",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-
-            // 설명
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                "Stanford University - The campus is really wide and beautiful.\nThe weather was so nice and it was a very happy day 🌞",
-              ),
-            ),
-
-            // 날짜
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 5, 0, 10),
-              child: Text(
-                "January 1",
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            )
+                ))
           ],
-        );
-      },
+        ),
+
+        // 좋아요
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            "268 likes",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+
+        // 설명
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            "Stanford University - The campus is really wide and beautiful.\nThe weather was so nice and it was a very happy day 🌞",
+          ),
+        ),
+
+        // 날짜
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 0, 10),
+          child: Text(
+            "January 1",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 이미지 list
+    final List<String> images = [
+      "https://blogpfthumb-phinf.pstatic.net/MjAyMjA4MjZfMjc2/MDAxNjYxNDk3ODIzNTY2.C-68xdAph9gtJ0DFY8SlCg-EDIA9-luj2rxRRpfJI5gg.e3EDRAhef_1CcWWUesX0ey7oAfdfdY7zEbjE1e1chZYg.PNG.iwbm0312/profileImage.png",
+      "https://postfiles.pstatic.net/MjAyMzAxMDFfMjY0/MDAxNjcyNTAzMDgxNTgy.lLFr5oYu7HxMIq1_OimJNf4P0j0mxN26HGnPFhRvw10g.MqvXKEAvO3WHL3i1iORGkH7UwAA2XzRwSbMBzqJpwCIg.JPEG.iwbm0312/IMG_3441.jpg?type=w966",
+      "https://postfiles.pstatic.net/MjAyMzAxMDFfMTE1/MDAxNjcyNTAzMDc3ODIy.3gDjMn12Nn6T9XKbe1XjqjhUeFHMRqXvY0xVEzuBgNgg.73k12NHvRJbFX7rlkvFjivhWQNHc4LFWER0cPEubh9wg.JPEG.iwbm0312/IMG_4825.jpg?type=w966",
+      "https://postfiles.pstatic.net/MjAyMzAxMDFfMjUy/MDAxNjcyNTAzMDc2NTM2.yX4NveMZR2AE-eNAqkMWr0JB9y5NcDdaSx29Ue4JIfQg.KxUlenx27JW-Aerh2kUAbp5seo3lFwmMU6VNivSYcrIg.JPEG.iwbm0312/IMG_6870.JPG?type=w966",
+      "https://postfiles.pstatic.net/MjAyMzAxMDFfMTk0/MDAxNjcyNTAzMDgwMjE3.D72vNrs_NmwWY-rUQ9jPXyihsLp7jS4Jlu7KKKik7gkg.3GP6dzT0Hz0CFJ8u1RkZkmG5FO3lCi9DGE1AQ3iHBdsg.JPEG.iwbm0312/IMG_7284.jpg?type=w966",
+    ];
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -27,7 +36,15 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Feed(),
+      body: ListView.builder(
+        itemCount: images.length, // 이미지 리스트 원소 개수만큼 피드 보여주기
+        itemBuilder: (context, index) {
+          final image = images[index]; // index에 해당되는 이미지 경로 전달
+          return Feed(
+            imageUrl: image,
+          ); // imageUrl 전달
+        },
+      ),
     );
   }
 }
